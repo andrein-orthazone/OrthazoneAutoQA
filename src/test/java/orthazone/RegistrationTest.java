03b50a3ef13db2a47f14a6940cea53b1831bfba8
Nadi-Olesi <n.i.olesiyuk@gmail.com>
2023-04-28 13:13:21 +0200

    Reg test. Part 3

--- src/test/java/orthazone/RegistrationTest.java
+++ src/test/java/orthazone/RegistrationTest.java
@@ -1,5 +1,6 @@
 package orthazone;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
@@ -24,11 +25,42 @@ public class RegistrationTest extends BaseTest {
         getDriver().findElement(By.xpath("//input[@name='confirm']")).sendKeys("123456789");
         WebElement clickPersonalAccount = getDriver().findElement(By.xpath("//label[@for='personal']"));
         clickPersonalAccount.click();
-        Actions actions = new Actions(getDriver());
-        //actions.moveToElement(getDriver().findElement(By.xpath("//div[@class='y-aao-row__head']"))).build().perform();
+
+        ((JavascriptExecutor)getDriver()).executeScript("window.scrollBy(0,300)");
+
+//        Actions actions = new Actions(getDriver());
+//        actions.moveToElement(getDriver().findElement(By.xpath("//div[@class='y-aao-row__head']"))).build().perform();
         WebElement nextButton = getDriver().findElement(By.xpath("//button[@class='r-btn abtn abtn--next']"));
 
         nextButton.click();
+        Thread.sleep(1000);
+        getDriver().findElement(By.xpath("//div[@data-block-acc='personal']//input[@name='firstname']")).sendKeys("test");
+        getDriver().findElement(By.xpath("//div[@data-block-acc='personal']//input[@name='lastname']")).sendKeys("test");
+        nextButton.click();
+
+        //JavascriptExecutor js = (JavascriptExecutor) getDriver();
+        //getDriver().get("http://demo.guru99.com/test/guru99home/");
+        //js.executeScript("window.scrollBy(0,1500)");
+        //js.executeScript("arguments[0].scrollIntoView();", getDriver().findElement(By.xpath("//div[@class='y-aao-row__head']")));
+        //WebElement scroll = getDriver().findElement(By.xpath("//p[@style='text-align: left;']"));
+        ((JavascriptExecutor)getDriver()).executeScript("window.scrollBy(0,600)");
+
+        WebElement radioPrivacyPolicy = getDriver().findElement(By.xpath("//p[@style='text-align: left;']"));
+        radioPrivacyPolicy.click();
+//        WebElement registerButton = getDriver().findElement(By.xpath("//button[@class='r-btn abtn abtn--send is_show']"));
+//        registerButton.click();
+//
+//        WebElement successRegistration = getDriver().findElement(By.xpath("//div[@class='asteps__head']"));
+//        Assert.assertEquals(successRegistration,"YOUR ACCOUNT HAS BEEN CREATED!");
+
+
+    }
+
+    @Test
+    public void testScroll(){
+        WebElement scroll = getDriver().findElement(By.xpath("//div[@class='y-aao-row__head']"));
+        ((JavascriptExecutor)getDriver()).executeScript("window.scrollBy(0,10000)");
+
 
     }
 
